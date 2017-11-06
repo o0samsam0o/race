2399c6cd0a9a0536c982f97a2d61478dab5dbe0b
Sandy Stehr <sandy.stehr@gmail.com>
2017-11-06 09:27:36 +0100

    added entity extensions

---
+++ RedRace2/RedRace2/Group.swift
@@ -0,0 +1,14 @@
+//
+//  Group.swift
+//  RedRace2
+//
+//  Created by Sandy Stehr on 04.11.17.
+//  Copyright © 2017 Sandy Stehr. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Group: NSManagedObject {
+
+}
